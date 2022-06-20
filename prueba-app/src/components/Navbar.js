84f282733d7ceee86997726963ca6385002eaa29
Facundo Núñez <107522248+Pitu97@users.noreply.github.com>
2022-06-20 19:24:00 -0300

    Agrego carpeta components y el componente Navbar

---
+++ prueba-app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <header className='navbar'>
+        <h3>MAMBA</h3>
+        <button>Inicio</button>
+        <a scr="#">Nosotros</a>
+        <a src="#">Productos</a>
+    </header>
+  );
+}
+
+export default Navbar;
--- prueba-app/src/index.js
+++ prueba-app/src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Saludo from './Saludo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Saludo></Saludo>
   </React.StrictMode>
 );
 
