3c9c343683079c91e4a06ec034194be187c12016
Facundo Núñez <facundo.mnunez97@gmail.com>
2022-06-29 17:13:38 -0300

    Correcciones

--- prueba-app/src/components/ItemList.js
+++ prueba-app/src/components/ItemList.js
@@ -1,24 +1,14 @@
-import { useEffect, useState } from 'react';
 import Item from './Item.js';
 import './ItemList.css';
 
 
-function ItemList() {
-
-  const [info, setInfo] = useState([])
-
-  useEffect(() =>{
-    fetch('data.json')
-    .then((resp) => resp.json())
-    .then((data) => setTimeout(()=>{setInfo(data)}, 2000))
-  }, [])
-    
+function ItemList({item}) {  
   return (
     <><div>
         <h3>Catalogo de Productos</h3>
     </div>
     <div className='Cat'>
-        {info && info.map(item => <Item nombre={item.nombre} precio={item.precio} stock={item.stock} />)}
+        {item && item.map(x => <Item nombre={x.nombre} precio={x.precio} stock={x.stock} />)}
     </div>
     </>
   );
--- prueba-app/src/components/ItemListContainer.js
+++ prueba-app/src/components/ItemListContainer.js
@@ -1,13 +1,22 @@
+import { useEffect, useState } from 'react';
 import ItemList from './ItemList.js';
 import './ItemListContainer.css';
 
 
 function ItemListContainer({ greeting }) {
+  const [info, setInfo] = useState([])
+
+  useEffect(() =>{
+    fetch('data.json')
+    .then((resp) => resp.json())
+    .then((data) => setTimeout(()=>{setInfo(data)}, 2000))
+  }, [])
+
   return (
     <section className='Catalogo'>
       <h2>{greeting}</h2>
       <div className='List'>
-        <ItemList />    
+        <ItemList item={info}/>    
       </div>
     </section>
   );
