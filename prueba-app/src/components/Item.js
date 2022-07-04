772f161e56412522b712f9b1d3ded5d7842c35f4
Facundo Núñez <facundo.mnunez97@gmail.com>
2022-07-04 19:33:47 -0300

    correcciones

--- prueba-app/src/components/Item.js
+++ prueba-app/src/components/Item.js
@@ -3,13 +3,19 @@ import ItemCount from './ItemCount';
 
 
 function Item(props) {
+  const Add =(num) => {
+    if(props.stock !== 0 && num <= props.stock) {
+      console.log(num);
+    }
+  }
+
   return (
     <div className='Card'>
     <h4>{props.nombre}</h4>
     <p className='Datos'>{props.precio}</p>
     <p className='Datos'>Stock = {props.stock}</p>
     <button className='Detalle'>Ver Mas</button>
-    <ItemCount stock={props.stock} initial={1} />
+    <ItemCount stock={props.stock} initial={1} onAdd={Add}/>
     </div>
   );
 }
--- prueba-app/src/components/ItemCount.js
+++ prueba-app/src/components/ItemCount.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './ItemCount.css';
 
 
-function ItemCount({ stock, initial }) {
+function ItemCount({ stock, initial, onAdd }) {
   const [ num, setNum ] = useState(initial)
 
   const suma = () => {
@@ -16,18 +16,14 @@ function ItemCount({ stock, initial }) {
       setNum(num - 1);
     }
   }
-  const onAdd =() => {
-    if(stock !== 0 && num <= stock) {
-      console.log(num);
-    }
-  }
+  
 
   return (
     <><div className="Counter">
       <button onClick={resta} className="Botones">-</button>
       <p>{num}</p>
       <button onClick={suma} className="Botones">+</button>
-    </div><button onClick={onAdd} className="Agregar">Agregar al Carrito</button></>
+    </div><button onClick={onAdd(num)} className="Agregar">Agregar al Carrito</button></>
   );
 }
 
