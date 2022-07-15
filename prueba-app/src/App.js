673aaa637b9ac81979e608294f2b57893bafd298
Facundo Núñez <facundo.mnunez97@gmail.com>
2022-07-15 14:46:05 -0300

    context

--- prueba-app/src/App.js
+++ prueba-app/src/App.js
@@ -5,21 +5,24 @@ import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer';
 import Navbar from './components/Navbar.js';
 import NotFound from './components/NotFound';
+import CartContext from './context/CartContext';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className='Catalogo'>
-        <Routes>
-          <Route path="/" element={<ItemListContainer />}></Route>
-          <Route path="/category/:id" element={<ItemListContainer />}></Route> 
-          <Route path="/item/:idItem" element={<ItemDetailContainer />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
+    <CartContext.Provider>
+      <div className="App">
+        <Navbar />
+        <div className='Catalogo'>
+          <Routes>
+            <Route path="/" element={<ItemListContainer />}></Route>
+            <Route path="/category/:id" element={<ItemListContainer />}></Route> 
+            <Route path="/item/:idItem" element={<ItemDetailContainer />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </CartContext.Provider>
   );
 }
 
--- prueba-app/src/components/ItemDetail.js
+++ prueba-app/src/components/ItemDetail.js
@@ -27,7 +27,7 @@ function ItemDetail({item}) {
           <p>{item.descripcion}</p>
           <p>Stock: {item.stock}</p>
           <div className='botones'>
-            {!added && <Link to="/cart"><button className='boton'>Comprar</button></Link>}
+            {/* {!added && <Link to="/cart"><button className='boton'>Comprar</button></Link>} */}
             {added && <Link to="/cart"><button className='boton2'>Terminar Compra</button></Link>}
             {!added && <ItemCount stock={item.stock} initial={1} onAdd={Add}/>}
             {added && <p>Producto agregado al carrito!</p>}
