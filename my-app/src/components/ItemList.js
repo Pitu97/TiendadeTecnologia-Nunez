4bf622c0bb0040be25461e5aa64d1f5812054bce
Facundo Núñez <facundo.mnunez97@gmail.com>
2022-08-09 00:15:03 -0300

    Cambios

--- my-app/src/components/ItemList.js
+++ my-app/src/components/ItemList.js
@@ -2,13 +2,13 @@ import Item from './Item.js';
 import './ItemList.css';
 
 
-function ItemList({items}, {idItem}) {  
+function ItemList({items}) {  
   return (
     <><div>
         <h3 className='categoria'>Catalogo de Productos</h3>
     </div>
     <div className='Cat'>
-        {items && items.map(item => <Item img2={item.img2} nombre={item.nombre} precio={item.precio} stock={item.stock} id={idItem} />)}
+        {items && items.map(item => <Item img2={item.img2} nombre={item.nombre} precio={item.precio} stock={item.stock} id={item.id} />)}
     </div>
     </>
   );
--- my-app/src/components/ItemListContainer.js
+++ my-app/src/components/ItemListContainer.js
@@ -2,39 +2,43 @@ import { useEffect, useState } from 'react';
 import ItemList from './ItemList.js';
 import './ItemListContainer.css';
 import { useParams } from 'react-router-dom';
-import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
-import datos from '../components/data/datos';
+import { getFirestore, collection, getDocs, addDoc, setDoc, doc } from 'firebase/firestore';
+//import datos from '../components/data/datos';
 
 
 function ItemListContainer() {
   const [info, setInfo] = useState([])
   const {cat} = useParams();
   const [data, setData] = useState([])
-  const [idItem, setIdItem] = useState();
+  const [idItem, setIdItem] = useState("");
+  const base = getFirestore();
   //console.log(id);
 
-  const isInData = (id) => {
-    return data.find((e) => e.id === id);
-  }
+  
 
-  const base = getFirestore();
+  //  const isInData = (id) => {
+  //    const q = collection(base, "items");
+  //      getDocs(q).then((items) => {
+  //        const copy = items.docs.map((doc) => doc.data());
+  //      return (copy.find((e) => e.id === id))
+  //      })
+  //  }
+    
+  // useEffect(() => { 
+  //   const q = collection(base, "items");
+  //   const promiseData = new Promise((resolve,reject) => {
+  //        resolve(datos)
+  //        }).then((dat) => setData(dat))
+  //    data.forEach((item) => {
+  //    //console.log(!isInData(item.id)) //CAMBIAR FILTRO 
+  //    if(!isInData(item.id)) {
+  //      addDoc(q, item).then((doc) => setIdItem(doc.id));
+  //    }
+  //  })   
+  //  }, [data])  
 
-  const q = collection(base, "items");
-        
   useEffect(() => {
-    const promiseData = new Promise((resolve,reject) => {
-        resolve(datos)
-        //console.log(datos);
-        }).then((dat) => setData(dat))
-    data.forEach((item) => {
-    console.log(isInData(item.id)) //CAMBIAR FILTRO 
-    if(!isInData(item.id)) {
-      addDoc(q, item).then((doc) => setIdItem(doc.id));
-    }
-  })   
-  }, [data])
-
-  useEffect(() => {
+    const q = collection(base, "items"); 
     getDocs(q).then((items) => {
         const i = items.docs.map((doc) => doc.data());
         switch(cat) {
@@ -62,7 +66,7 @@ function ItemListContainer() {
   return (
     <>
       <div className='List'>
-        <ItemList items={info} idItem={idItem}/>    
+        <ItemList items={info}/>    
       </div>
     </>
   );
