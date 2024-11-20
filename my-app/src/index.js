0366df9fcaddf4ab15e01aaf8b13c07cc3f2657a
Facundo Núñez <facundo.mnunez97@gmail.com>
2024-11-20 15:52:35 -0300

    creado gh-pages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -25,7 +25,7 @@ initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/TiendadeTecnologia-Nunez">
       <App />
     </BrowserRouter>
   </React.StrictMode>
